7e95a042100ba3ad6e9f09a88cc6ba177043fa6c
Caitlin Ash <caitlinba27@gmail.com>
2023-11-16 14:04:09 -0600

    Displayed current date at the top of the calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// the code isn't run until the browser has finished rendering all the elements in the html
 $(function () {
+  // variable for currentDay element
+  const currentDate = document.getElementById('currentDay');
+
+  // format the current date and display it
+  let today = dayjs().format('dddd, MMMM DD');
+  currentDate.textContent = today;
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,6 +13,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
